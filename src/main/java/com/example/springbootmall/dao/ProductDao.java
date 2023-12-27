77d14dca185fe2f7b5aac3b6d30095414a11c7bc
emily.jiang <emily.jiang@nec.com.tw>
2023-12-27 16:32:56 +0800

    Revert "新增商品"
    This reverts commit cdb1dfe65dce4d0c34929c92fe89b32306135f59.

--- src/main/java/com/example/springbootmall/dao/ProductDao.java
+++ src/main/java/com/example/springbootmall/dao/ProductDao.java
@@ -8,6 +8,4 @@ public interface ProductDao {
 	Product getProductById(Integer productId);
 
 	Integer createProduct(ProductRequest productRequest);
-
-	void updateProduct(Integer productId, ProductRequest productRequest);
 }
--- src/main/java/com/example/springbootmall/service/ProductService.java
+++ src/main/java/com/example/springbootmall/service/ProductService.java
@@ -9,6 +9,4 @@ public interface ProductService {
 	Product getProductById(Integer productId);
 
 	Integer createProduct(ProductRequest productRequest);
-
-	void updateProduct(Integer productId, ProductRequest productRequest);
 }
