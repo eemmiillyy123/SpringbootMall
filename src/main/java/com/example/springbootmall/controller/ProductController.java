cdb1dfe65dce4d0c34929c92fe89b32306135f59
emily.jiang <emily.jiang@nec.com.tw>
2023-12-27 16:32:03 +0800

    新增商品

--- src/main/java/com/example/springbootmall/controller/ProductController.java
+++ src/main/java/com/example/springbootmall/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +36,19 @@ public class ProductController {
 		Integer productId= productService.createProduct(productRequest);
 		Product product=productService.getProductById(productId);
 		return ResponseEntity.status(HttpStatus.CREATED).body(product);
+	}
+	
+	@PutMapping("/products/{productId}")
+	public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,@RequestBody @Valid ProductRequest productRequest){
+		Product  product=productService.getProductById(productId);
+		//查詢商品是否存在，存在的話才去更新商品
+		if(product==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		//修改商品數據
+		productService.updateProduct(productId,productRequest);
+		Product updateProduct=productService.getProductById(productId);//取得更新後的數據
+		return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+		
 	}
 }
